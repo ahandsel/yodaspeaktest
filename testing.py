5fffc8d192862a995cd9122dc6c2d1e498332ccb
ahandsel <ahandsel@gmail.com>
2017-08-17 23:03:54 -0700

    update

---
+++ testing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Do Yoda Speak
+
+Takes text and converts it into something Yoda might say
+"""
+
+import requests
+
+req = requests.request('GET', 'https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.',
+  headers={
+      "X-Mashape-Key": "Upi6oCn4eRmshBVdnoRikxZLfAqkp17iLWzjsnisxHux3sNAnC",
+      "Accept": "text/plain"
+  }
+)
+#code so "sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait." is a seperate paramenters
+print(req)
+
+
+#
+#
+# Key: Upi6oCn4eRmshBVdnoRikxZLfAqkp17iLWzjsnisxHux3sNAnC
+#
+#
+# response = unirest.get("",
+#   headers={
+#     "X-Mashape-Key": "Upi6oCn4eRmshBVdnoRikxZLfAqkp17iLWzjsnisxHux3sNAnC",
+#     "Accept": "text/plain"
+#   }
+# )
